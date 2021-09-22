c35775a2a67cd94d6a17fcb3cdb573124e913387
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-22 16:19:30 +0200

    Születési hónapot, napot és életkort integerként visszaadó függvény létrehozása

--- src/hu/petrik/emberekoop/Ember.java
+++ src/hu/petrik/emberekoop/Ember.java
@@ -1,5 +1,8 @@
 package hu.petrik.emberekoop;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Ember {
     private String nev;
     private String szulDatum;
@@ -14,6 +17,23 @@ public class Ember {
     public int getSzuletesiEv(){
         String szuletesiEvStringben = this.szulDatum.substring(0,4);
         return Integer.parseInt(szuletesiEvStringben);
+    }
+
+    public int getSzuletesiHonap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[1]);
+    }
+
+    public int getSzuletesiNap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[2]);
+    }
+
+    public int getEletkor(){
+        LocalDate maiDatum = LocalDate.now();
+        LocalDate szuletesiDatum = LocalDate.parse(szulDatum);
+        Period kulonbseg = Period.between(maiDatum, szuletesiDatum);
+        return kulonbseg.getYears();
     }
 
     @Override
