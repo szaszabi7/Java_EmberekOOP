5183f047f54fff288afa39541c6781e4cfd9ba53
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-22 15:53:32 +0200

    Tesztadat átnevezése

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -3,7 +3,7 @@ package hu.petrik.emberekoop;
 public class Main {
 
     public static void main(String[] args) {
-        Ember e1 = new Ember("Lakatos Ferenc", "1983-1-12", "Budapest");
+        Ember e1 = new Ember("Lakatos István", "1983-1-12", "Budapest");
         Ember e2 = new Ember("Gipsz Jakab", "1968-11-2", "Miskolc");
         Ember e3 = new Ember("Teszt Elek", "2001-8-22", "Kecskemét");
         System.out.println(e1);
