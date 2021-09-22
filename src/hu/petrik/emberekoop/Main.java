356f8bd1f85d9ca931b903021aada11bcf816072
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-22 15:49:57 +0200

    Ember osztály tesztelése

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -3,6 +3,12 @@ package hu.petrik.emberekoop;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Ember e1 = new Ember("Lakatos Ferenc", "1983-1-12", "Budapest");
+        Ember e2 = new Ember("Gipsz Jakab", "1968-11-2", "Miskolc");
+        Ember e3 = new Ember("Teszt Elek", "2001-8-22", "Kecskemét");
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
+
     }
 }
