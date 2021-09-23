3e402021aa5aac2d541642eb79ab16cc8b85bc3a
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-23 11:13:31 +0200

    Emberek osztályban konstruktor ami statikus többel tölti fel a listát

--- src/hu/petrik/emberekoop/Emberek.java
+++ src/hu/petrik/emberekoop/Emberek.java
@@ -1,7 +1,20 @@
 package hu.petrik.emberekoop;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Emberek {
     private List<Ember> emberLista;
+
+    public Emberek(Ember[] emberTomb){
+        this.emberLista = new ArrayList<>();
+        // Tömböt átalakítottuk listává, és az összes elemet az addAll függvény segítségével felvettük.
+        this.emberLista.addAll(Arrays.asList(emberTomb));
+        /*
+        for (Ember ember: emberTomb) {
+            this.emberLista.add(ember);
+        }
+         */
+    }
 }
