997e538b7c1a7ca78b5b1899357ccf9292e5c3aa
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-23 11:22:09 +0200

    Main osztályban korábbi kiíratások kihelyezése metódusba, és statikus tömb létrehozása

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -7,6 +7,15 @@ public class Main {
         Ember e1 = new Ember("Lakatos Béla", "1983-1-12", "Budapest");
         Ember e2 = new Ember("Gipsz Jakab", "1968-11-2", "Miskolc");
         Ember e3 = new Ember("Teszt Elek", "2001-8-22", "Kecskemét");
+        Ember e4 = new Ember("Kovács László", "1988-6-3", "Nyíregyháza");
+        //emberOsztalyTesztje(e0,e1,e2,e3);
+
+        Ember[] emberTomb = new Ember[]{e0,e1,e2,e3,e4};
+
+
+    }
+
+    public static void emberOsztalyTesztje(Ember e0, Ember e1, Ember e2, Ember e3){
         System.out.println(e1);
         System.out.println(e2);
         System.out.println(e3);
